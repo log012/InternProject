e5375c3fac60979849020df9ff21de717fac5dd1
log012 <142375405+log012@users.noreply.github.com>
2024-02-28 11:56:48 +0530

    redirct issue solved

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Laravel\Socialite\Two\FacebookProvider;
 
 Route::get('/home', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -30,17 +30,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/admin/dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
+Route::get('/admin/developer',[AdminController::class,'developer'])->name('admin.developer');
+Route::get('/admin/home',[AdminController::class,'home'])->name('admin.home'); 
+Route::get('/admin/lead_data',[AdminController::class,'read_lead_message']);
+Route::post('/logout',[UserController::class,'logout'])->middleware('auth')->name('logout');
+Route::get('/admin/user_detail',[AdminController::class,'getFacebookId'])->name('admin.user_detail');
 });
 
 require __DIR__.'/auth.php';
 // admin
-Route::get('/admin/dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
-Route::get('/admin/developer',[AdminController::class,'developer'])->name('admin.developer');
-Route::get('/admin/home',[AdminController::class,'home'])->name('admin.home'); 
-Route::get('/admin/lead_data',[AdminController::class,'read_lead_message']);
-Route::get('/admin/user_detail',[AdminController::class,'getFacebookId'])->name('admin.user_detail'); // add /{name} to the route
+ // add /{name} to the route
 // Facebook
 Route::get('/facebook/login',[UserController::class,'userLogin']);
 Route::get('/facebook/callback',[UserController::class,'userCallback']);
-Route::post('/logout',[AuthenticatedSessionController::class,'destroy'])->name('logout')->middleware('auth');
+// Route::post('/logout',[AuthenticatedSessionController::class,'destroy'])->name('logout')->middleware('auth');
 
